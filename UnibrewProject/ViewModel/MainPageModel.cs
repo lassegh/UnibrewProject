fd9a043e68b7659ab37a5e20e8bb01d0a7fb04d4
lassegh <lasse.groenbech@gmail.com>
2019-04-23 13:16:54 +0200

    Initial commit

---
+++ UnibrewProject/ViewModel/MainPageModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using UnibrewProject.Annotations;
+
+namespace UnibrewProject.ViewModel
+{
+    class MainPageModel : INotifyPropertyChanged
+    {
+        public MainPageModel()
+        {
+            
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
