f8b997a3d9b0a4ac551db27207980924e2be6101
LarsDuJour <42436050+LarsDuJour@users.noreply.github.com>
2019-04-24 11:45:13 +0200

    REST service v0.1
    REST lavet
    SQLconnectionSingleton lavet
    interface lavet
    
    Alt i stil med Peters arbejde

---
+++ UnibrewREST/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace UnibrewREST.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ UnibrewREST/DButil/IManage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace UnibrewREST
+{
+    public interface IManage<T>
+    {
+        IEnumerable<T> Get();
+        T Get(int id);
+        bool Post(T elem);
+        bool Put(int id, T elem);
+        bool Delete(int id);
+    }
+}
