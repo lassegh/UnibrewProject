29edee9eb5eab1eedd4e63dc7064dd04c542d212
lassegh <lasse.groenbech@gmail.com>
2019-04-26 13:32:11 +0200

    Fixed size

--- UnibrewProject/MainPage.xaml.cs
+++ UnibrewProject/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,12 @@ namespace UnibrewProject
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void FormName_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
+            ApplicationView.GetForCurrentView().TryResizeView(new Size(1080, 720));
         }
     }
 }
