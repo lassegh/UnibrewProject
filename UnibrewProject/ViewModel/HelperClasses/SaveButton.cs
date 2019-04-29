36d986aa58b6bae285cd5b96861999f46e905933
lassegh <lasse.groenbech@gmail.com>
2019-04-29 10:27:59 +0200

    SaveUpdate

--- UnibrewProject/ViewModel/HelperClasses/SaveButton.cs
+++ UnibrewProject/ViewModel/HelperClasses/SaveButton.cs
@@ -20,5 +20,20 @@ namespace UnibrewProject.ViewModel.HelperClasses
         }
 
         public RelayCommand SaveCommand { get; set; }
+        public double Bottle01 { get; set; }
+        public double Bottle02 { get; set; }
+        public double Bottle03 { get; set; }
+        public double Bottle04 { get; set; }
+        public double Bottle05 { get; set; }
+        public double Bottle06 { get; set; }
+        public double Bottle07 { get; set; }
+        public double Bottle08 { get; set; }
+        public double Bottle09 { get; set; }
+        public double Bottle10 { get; set; }
+        public double Bottle11 { get; set; }
+        public double Bottle12 { get; set; }
+        public double Bottle13 { get; set; }
+        public double Bottle14 { get; set; }
+        public double Bottle15 { get; set; }
     }
 }
