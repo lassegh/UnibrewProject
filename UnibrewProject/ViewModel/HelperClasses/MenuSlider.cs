84a4752b2da2989a6b1535835454486f0c6aab73
lassegh <lasse.groenbech@gmail.com>
2019-04-29 08:39:00 +0200

    Refactored ViewModel

---
+++ UnibrewProject/ViewModel/HelperClasses/MenuSlider.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+using GalaSoft.MvvmLight.Command;
+using UnibrewProject.Annotations;
+
+namespace UnibrewProject.ViewModel.HelperClasses
+{
+    public class MenuSlider : INotifyPropertyChanged
+    {
+        private delegate void CallBack(Object state);
+
+        private CallBack callBack;
+
+        private int _menuWidth = 50;
+        private bool _menuTextVisibility = false;
+
+        public MenuSlider()
+        {
+            MenuHideButton = new RelayCommand(MenuHideMethod);
+            callBack = MenuShowCallback;
+
+        }
+
+        private void MenuHideMethod()
+        {
+            MenuMoveTimer = new Timer(new TimerCallback(callBack), null, 1, Timeout.Infinite);
+
+        }
+
+        private void MenuShowCallback(Object state)
+        {
+            if (MenuWidth < 200)
+            {
+
+                MenuWidth = MenuWidth + 5;
+                MenuMoveTimer.Change(1, Timeout.Infinite);
+            }
+            else
+            {
+                MenuMoveTimer.Dispose();
+                callBack = MenuHideCallback;
+                MenuTextVisibility = true;
+            }
+        }
+
+        private void MenuHideCallback(Object state)
+        {
+            if (MenuWidth > 50)
+            {
+                MenuTextVisibility = false;
+                MenuWidth = MenuWidth - 5;
+                MenuMoveTimer.Change(1, Timeout.Infinite);
+            }
+            else
+            {
+                MenuMoveTimer.Dispose();
+                callBack = MenuShowCallback;
+
+            }
+
+        }
+
+        public Timer MenuMoveTimer { get; set; }
+        public RelayCommand MenuHideButton { get; set; }
+
+        public int MenuWidth
+        {
+            get { return _menuWidth; }
+            set
+            {
+                _menuWidth = value;
+                Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                    () =>
+                    {
+                        // Your UI update code goes here!
+                        OnPropertyChanged();
+                    }
+                );
+            }
+        }
+
+        public bool MenuTextVisibility
+        {
+            get { return _menuTextVisibility; }
+            set
+            {
+                _menuTextVisibility = value;
+                Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                    () =>
+                    {
+                        // Your UI update code goes here!
+                        OnPropertyChanged();
+                    }
+                );
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- UnibrewProject/ViewModel/MainPageModel.cs
+++ UnibrewProject/ViewModel/MainPageModel.cs
@@ -14,25 +14,17 @@ using GalaSoft.MvvmLight.Command;
 using UnibrewProject.Annotations;
 using LiveCharts;
 using LiveCharts.Uwp;
+using UnibrewProject.ViewModel.HelperClasses;
 
 namespace UnibrewProject.ViewModel
 {
-    class MainPageModel : INotifyPropertyChanged
+    class MainPageModel
     {
-
-        private delegate void CallBack(Object state);
-
-        private CallBack callBack;
-
-        private int _menuWidth = 50;
-        private bool _menuTextVisibility = false;
 
         public MainPageModel()
         {
+            Slider = new MenuSlider();
             
-            MenuHideButton = new RelayCommand(MenuHideMethod);
-            callBack = MenuShowCallback;
-
             SeriesCollection = new SeriesCollection
             {
                 new LineSeries
@@ -73,96 +65,10 @@ namespace UnibrewProject.ViewModel
             SeriesCollection[3].Values.Add(5d);*/
         }
 
-        private void MenuHideMethod()
-        {
-            MenuMoveTimer = new Timer(new TimerCallback(callBack),null,1,Timeout.Infinite);
-           
-        }
-
-        private void MenuShowCallback(Object state)
-        {
-            if (MenuWidth < 200)
-            {
-                
-                MenuWidth = MenuWidth + 5;
-                MenuMoveTimer.Change(1, Timeout.Infinite);
-            }
-            else
-            {
-                MenuMoveTimer.Dispose();
-                callBack = MenuHideCallback;
-                MenuTextVisibility = true;
-            }
-        }
-
-        private void MenuHideCallback(Object state)
-        {
-            if (MenuWidth > 50)
-            {
-                MenuTextVisibility = false;
-                MenuWidth = MenuWidth - 5;
-                MenuMoveTimer.Change(1, Timeout.Infinite);
-            }
-            else
-            {
-                MenuMoveTimer.Dispose();
-                callBack = MenuShowCallback;
-                
-            }
-            
-        }
-
-        public Timer MenuMoveTimer { get; set; }
-        public RelayCommand MenuHideButton { get; set; }
-
         public SeriesCollection SeriesCollection { get; set; }
         public string[] Labels { get; set; }
         public Func<double, string> YFormatter { get; set; }
 
-
-        #region MenuWidth
-        public int MenuWidth
-        {
-            get { return _menuWidth; }
-            set
-            {
-                _menuWidth = value;
-                Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
-                    () =>
-                    {
-                        // Your UI update code goes here!
-                        OnPropertyChanged();
-                    }
-                );
-            }
-        }
-        #endregion
-
-        #region MenuTextVisability
-        public bool MenuTextVisibility
-        {
-            get { return _menuTextVisibility; }
-            set
-            {
-                _menuTextVisibility = value;
-                Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
-                    () =>
-                    {
-                        // Your UI update code goes here!
-                        OnPropertyChanged();
-                    }
-                );
-            }
-        }
-        #endregion
-
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        [NotifyPropertyChangedInvocator]
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        public MenuSlider Slider { get; set; }
     }
 }
