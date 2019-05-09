a52b48fb5c7cbb99a137c057eeed41cdf7ce3347
lassegh <lasse.groenbech@gmail.com>
2019-05-09 08:51:54 +0200

    FinishedItem

--- UnibrewProject/ViewModel/InputColSevenViewModel.cs
+++ UnibrewProject/ViewModel/InputColSevenViewModel.cs
@@ -9,28 +9,21 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using GalaSoft.MvvmLight.Command;
 using UnibrewProject.Annotations;
+using UnibrewProject.Model;
 using UnibrewProject.ViewModel.HelperClasses;
+using UnibrewProject.ViewModel.HelperClasses.DbCommunication;
 
 namespace UnibrewProject.ViewModel
 {
     public class InputColSevenViewModel : INotifyPropertyChanged
     {
-        
-        public MenuSlider Slider { get; set; }
-        public MenuNavigator Navigator { get; set; }
-        public SaveTapOperator Save { get; set; } = SaveTapOperator.Save;
-
-        public RelayCommand<object> RelayCommand_inputValid { get; set; }
 
         public double txt_out;
         private string _txtbxInputValid;
-
-        public string txtbx_inputValid
-        {
-            get => _txtbxInputValid;
-            set { _txtbxInputValid = value; OnPropertyChanged();}
-        }
-
+        private IEnumerable<FinishedItems> _currentFinishedItem;
+        private string _finishedItemNumber;
+        private DbComFinishedItems _dbComFinishedItems;
+        
         public InputColSevenViewModel()
         {
             Slider = new MenuSlider();
@@ -57,6 +50,40 @@ namespace UnibrewProject.ViewModel
 
             _txtbxInputValid = box?.Text;
 
+        }
+
+        public MenuSlider Slider { get; set; }
+        public MenuNavigator Navigator { get; set; }
+        public SaveTapOperator Save { get; set; } = SaveTapOperator.Save;
+
+        public RelayCommand<object> RelayCommand_inputValid { get; set; }
+
+        public string txtbx_inputValid
+        {
+            get => _txtbxInputValid;
+            set { _txtbxInputValid = value; OnPropertyChanged(); }
+        }
+
+        public IEnumerable<FinishedItems> CurrentFinishedItem
+        {
+            get { return _currentFinishedItem; }
+            set
+            {
+                _currentFinishedItem = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string FinishedItemNumber
+        {
+            get { return _finishedItemNumber; }
+            set
+            {
+                _finishedItemNumber = value;
+                int i;
+                if (!int.TryParse(value, out i)) i = 0;
+                CurrentFinishedItem = _dbComFinishedItems.FinishedItemsList.Where(n => n.FinishedItemNumber.Equals(i));
+            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
