4a6e8bad66c47d0a246fc79bd8786e43a9e1f18e
lassegh <lasse.groenbech@gmail.com>
2019-05-10 11:40:01 +0200

    Binding LiquidTanks

--- UnibrewProject/App.xaml.cs
+++ UnibrewProject/App.xaml.cs
@@ -74,7 +74,7 @@ namespace UnibrewProject
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(InputColSevenPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
