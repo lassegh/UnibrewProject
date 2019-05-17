2d66b7aba1e5d6bd539058bdeca564889a5cda93
lassegh <lasse.groenbech@gmail.com>
2019-05-17 09:09:14 +0200

    DatePicker Graph

---
+++ UnibrewProject/ViewModel/HelperClasses/StatisticsBuilder.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Media3D;
+using LiveCharts;
+using LiveCharts.Uwp;
+using UnibrewProject.Annotations;
+using UnibrewProject.Model;
+
+namespace UnibrewProject.ViewModel.HelperClasses
+{
+    public class StatisticsBuilder : INotifyPropertyChanged
+    {
+        private SeriesCollection _momentCollection;
+        private List<string> _momentLabels;
+        private Func<double, string> _yFormatter;
+
+        public StatisticsBuilder(List<TapOperator> tapOperatorList, DateTime startDate, DateTime endDate)
+        {
+            RebiuldStats(tapOperatorList,startDate,endDate);
+        }
+
+        public void RebiuldStats(List<TapOperator> tapOperatorList, DateTime startDate, DateTime endDate)
+        {
+            List<double> bottleOne = new List<double>();    
+            List<double> bottleTwo = new List<double>();
+            List<double> bottleThree = new List<double>();
+            MomentLabels = new List<string>();
+            foreach (TapOperator tapOperator in tapOperatorList)
+            {
+                if (tapOperator.ClockDate > startDate && tapOperator.ClockDate < endDate)
+                {
+                    bottleOne.Add(tapOperator.Bottle1);
+                    bottleTwo.Add(tapOperator.Bottle2);
+                    bottleThree.Add(tapOperator.Bottle3);
+                    MomentLabels.Add(tapOperator.ClockDate.ToString("dd-MM-yyyy HH:mm"));
+                }
+            }
+
+
+            MomentCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Flaske 1",
+                    Values = new ChartValues<double>(bottleOne) {}
+                },
+                new LineSeries
+                {
+                    Title = "Flaske 2",
+                    Values = new ChartValues<double>(bottleTwo) {},
+                },
+                new LineSeries
+                {
+                    Title = "Flaske 3",
+                    Values = new ChartValues<double>(bottleThree) {},
+                }
+            };
+            YFormatter = value => value.ToString("N");
+        }
+
+
+
+        public SeriesCollection MomentCollection
+        {
+            get { return _momentCollection; }
+            set
+            {
+                _momentCollection = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public List<string> MomentLabels
+        {
+            get { return _momentLabels; }
+            set
+            {
+                _momentLabels = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Func<double, string> YFormatter
+        {
+            get { return _yFormatter; }
+            set
+            {
+                _yFormatter = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- UnibrewProject/ViewModel/StatViewModel.cs
+++ UnibrewProject/ViewModel/StatViewModel.cs
@@ -1,7 +1,8 @@
 ﻿    using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
+    using System.Diagnostics;
+    using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
@@ -9,40 +10,76 @@ using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.UI.Core;
 using Windows.UI.ViewManagement;
-using Windows.UI.Xaml.Media;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Media;
 using GalaSoft.MvvmLight.Command;
 using UnibrewProject.Annotations;
-using UnibrewProject.ViewModel.HelperClasses;
+    using UnibrewProject.Model;
+    using UnibrewProject.ViewModel.HelperClasses;
 
 namespace UnibrewProject.ViewModel
 {
     class StatViewModel
     {
-        private DateTime _fromDateTime = DateTime.Today;
+        private List<TapOperator> _tapOperators = Loader.Load.GetTapOperators();
+        private DateTime _fromDateTime;
         private DateTime _toDateTime;
 
         public StatViewModel()
         {
             Slider = new MenuSlider();
             Navigator = new MenuNavigator();
-            Load.GetTapOperators();
-            
+            StatBuilder = new StatisticsBuilder(_tapOperators,FromDateTime,ToDateTime);
+            FromDateTime = new DateTime();
+            CalendarCommand = new RelayCommand<object>(CalendarCommandMethod);
+            CalendarToDateCommand = new RelayCommand<object>(CalendarToDateCommandMethod);
+        }
+
+        private void CalendarCommandMethod(object obj)
+        {
+            CalendarDatePickerDateChangedEventArgs args = obj as CalendarDatePickerDateChangedEventArgs;
+            if (args?.NewDate != null)
+            {
+                FromDateTime = (DateTime) args.NewDate.Value.DateTime;
+            }
+        }
+
+        private void CalendarToDateCommandMethod(object obj)
+        {
+            CalendarDatePickerDateChangedEventArgs args = obj as CalendarDatePickerDateChangedEventArgs;
+            if (args?.NewDate != null)
+            {
+                ToDateTime = (DateTime)args.NewDate.Value.DateTime;
+            }
         }
 
         public MenuSlider Slider { get; set; }
         public MenuNavigator Navigator { get; set; }
+        public StatisticsBuilder StatBuilder { get; set; }
         public Loader Load { get; set; } = Loader.Load;
+
+        public RelayCommand<object> CalendarCommand { get; set; }
+        public RelayCommand<object> CalendarToDateCommand { get; set; }
 
         public DateTime FromDateTime
         {
             get { return _fromDateTime; }
-            set { _fromDateTime = value; }
+            set
+            {
+                _fromDateTime = value;
+                StatBuilder.RebiuldStats(_tapOperators,FromDateTime,ToDateTime);
+            }
         }
 
         public DateTime ToDateTime
         {
             get { return _toDateTime; }
-            set { _toDateTime = value; }
+            set
+            {
+                _toDateTime = value;
+                StatBuilder.RebiuldStats(_tapOperators, FromDateTime, ToDateTime);
+            }
         }
     }
 }
