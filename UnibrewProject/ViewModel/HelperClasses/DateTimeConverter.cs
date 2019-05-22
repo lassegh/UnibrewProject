69d52bbb1a8b8ebdfc75fa146aebbb53fa79ec4e
lassegh <lasse.groenbech@gmail.com>
2019-05-22 16:38:41 +0200

    Calendar Fixed

---
+++ UnibrewProject/ViewModel/HelperClasses/DateTimeConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace UnibrewProject.ViewModel.HelperClasses
+{
+    public class DateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            DateTime d = (DateTime)value;
+
+            return new DateTimeOffset(d.Year, d.Month, d.Day, d.Hour, d.Minute, d.Second, new TimeSpan());
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return new DateTime(((DateTimeOffset)value).Ticks);
+
+        }
+    }
+}
